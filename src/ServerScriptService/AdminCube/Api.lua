74fbf2b3b71a46d68d22f534693d9a9a32e28dd7
Stonetr03 <mrstonetr03@gmail.com>
2021-07-31 23:37:59 -0700

    Name Matcher V2
    Name Matcher V2

--- src/ServerScriptService/AdminCube/Api.lua
+++ src/ServerScriptService/AdminCube/Api.lua
@@ -1,6 +1,7 @@
 -- Admin Cube - Server Api
 
 local DataStore = require(script.Parent:WaitForChild("DataStore"))
+local Settings = require(script.Parent:WaitForChild("Settings"))
 
 local Module = {}
 local Commands = {}
@@ -14,7 +15,20 @@ function Module:GetPlayer(Name,p) -- Name Requested to Find, Player who Sent
     if Name == "me" then
         return p
     else
-        
+        -- Match UserNames
+        for _,o in pairs(game.Players:GetPlayers()) do
+            if string.lower(o.Name) == Name then
+                return o
+            end
+        end
+        -- Match Display Names
+        if Settings.DisplayNames == true then
+            for _,o in pairs(game.Players:GetPlayers()) do
+                if string.lower(o.DisplayName) == Name then
+                    return o
+                end
+            end
+        end
     end
 end
 

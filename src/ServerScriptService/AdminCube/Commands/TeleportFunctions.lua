25305dfd87cee26058bd7f654d613f7b2313b0e6
Stonetr03 <mrstonetr03@gmail.com>
2022-08-12 16:33:25 -0700

    Update TeleportFunctions.lua
    Fixed Bugs, Waiting to be tested
    Applying to Admin-Cube Nightly

--- src/ServerScriptService/AdminCube/Commands/TeleportFunctions.lua
+++ src/ServerScriptService/AdminCube/Commands/TeleportFunctions.lua
@@ -7,10 +7,10 @@ Api:RegisterCommand("teleport","Teleports a player.",function(p,Args)
         if Api:GetRank(p) >= 2 then
             local Target = Api:GetPlayer(Args[1],p)
             if Args[2] == nil then
-                p.Character.HumanoidRootPart.Position = Target.Character.HumanoidRootPart.Position
+                p.Character:SetPrimaryPartCFrame(Target.Character.HumanoidRootPart.CFrame)
             else
                 local Target2 = Api:GetPlayer(Args[2],p)
-                Target.HumanoidRootPart.Position = Target2.HumanoidRootPart.Position
+                Target.Character:SetPrimaryPartCFrame(Target2.Character.HumanoidRootPart.CFrame)
             end
         else
             -- Invalid Rank Notification
@@ -28,10 +28,10 @@ Api:RegisterCommand("tp","Teleports a player.",function(p,Args)
         if Api:GetRank(p) >= 2 then
             local Target = Api:GetPlayer(Args[1],p)
             if Args[2] == nil then
-                p.Character.HumanoidRootPart.Position = Target.Character.HumanoidRootPart.Position
+                p.Character:SetPrimaryPartCFrame(Target.Character.HumanoidRootPart.CFrame)
             else
                 local Target2 = Api:GetPlayer(Args[2],p)
-                Target.HumanoidRootPart.Position = Target2.HumanoidRootPart.Position
+                Target.Character:SetPrimaryPartCFrame(Target2.Character.HumanoidRootPart.CFrame)
             end
         else
             -- Invalid Rank Notification
@@ -48,7 +48,7 @@ Api:RegisterCommand("bring","Teleports player to calling player.",function(p,Arg
     local s,e = pcall(function()
         if Api:GetRank(p) >= 2 then
             local Target = Api:GetPlayer(Args[1],p)
-            Target.Character.HumanoidRootPart.Position = p.Character.HumanoidRootPart.Position
+            Target.Character:SetPrimaryPartCFrame(p.Character.HumanoidRootPart.CFrame)
         else
             -- Invalid Rank Notification
             Api:InvalidPermissionsNotification(p)
@@ -63,7 +63,7 @@ Api:RegisterCommand("goto","Teleports player to calling player.",function(p,Args
     local s,e = pcall(function()
         if Api:GetRank(p) >= 2 then
             local Target = Api:GetPlayer(Args[1],p)
-            p.Character.HumanoidRootPart.Position = Target.Character.HumanoidRootPart.Position
+            p.Character:SetPrimaryPartCFrame(Target.Character.HumanoidRootPart.CFrame)
         else
             -- Invalid Rank Notification
             Api:InvalidPermissionsNotification(p)
@@ -79,7 +79,7 @@ Api:RegisterCommand("summon","Teleports all players to calling player.",function
         if Api:GetRank(p) >= 2 then
             for _,o in pairs(game.Players:GetPlayers()) do
                 pcall(function()
-                    o.Character.HumanoidRootPart.Position = p.Character.HumanoidRootPart.Position
+                    o.Character:SetPrimaryPartCFrame(p.Character.HumanoidRootPart.CFrame)
                 end)
             end
         else

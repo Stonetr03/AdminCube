d4b95e226fc1080b2df28de82c5c2b6480f0f688
Stonetr03 <mrstonetr03@gmail.com>
2022-08-22 22:58:16 -0700

    Update ban.lua

--- src/ServerScriptService/AdminCube/Commands/ban.lua
+++ src/ServerScriptService/AdminCube/Commands/ban.lua
@@ -3,10 +3,25 @@
 local Api = require(script.Parent.Parent:WaitForChild("Api"))
 local DataStore = require(script.Parent.Parent:WaitForChild("DataStore"))
 
+local cache = {} -- https://developer.roblox.com/en-us/api-reference/function/Players/GetUserIdFromNameAsync
+function getUserIdFromUsername(name)
+	if cache[name] then return cache[name] end
+	local player = game.Players:FindFirstChild(name)
+	if player then
+		cache[name] = player.UserId
+		return player.UserId
+	end 
+	local id
+	pcall(function ()
+		id = game.Players:GetUserIdFromNameAsync(name)
+	end)
+	cache[name] = id
+	return id
+end
+
 Api:RegisterCommand("ban","Opens ban prompt.",function(p,Args)
     local s,e = pcall(function()
         if Api:GetRank(p) >= 3 then
-            
 
              -- Show Prompt
              Api:ShowPrompt(p,{
@@ -41,6 +56,14 @@ Api:RegisterCommand("ban","Opens ban prompt.",function(p,Args)
                         
                         -- Todo
                         -- Check player name or Userid, Does not have to be in server.
+
+                        local UserId = getUserIdFromUsername(Response[2][1])
+                        if typeof(UserId) == "number" then
+                            -- Is userid
+
+                            -- New datastore functions GetRecord and UpdateRecord to change the datastore value of someone not in the server.
+
+                        end
 
                     end
 

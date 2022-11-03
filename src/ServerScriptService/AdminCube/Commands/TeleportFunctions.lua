943f5a1786cf0703bc5748742fba08b2df327640
Stonetr03 <mrstonetr03@gmail.com>
2022-11-02 19:11:57 -0700

    Fixed Alias for Tp
    + Fixed Alias for teleport/tp
    - Removed old tp command

--- src/ServerScriptService/AdminCube/Commands/TeleportFunctions.lua
+++ src/ServerScriptService/AdminCube/Commands/TeleportFunctions.lua
@@ -21,28 +21,7 @@ Api:RegisterCommand("teleport","Teleports a player.",function(p,Args)
         warn(e)
     end
 
-end,{"tp"})
-
---Api:RegisterCommand("tp","Teleports a player.",function(p,Args)
---    local s,e = pcall(function()
---        if Api:GetRank(p) >= 2 then
---            local Target = Api:GetPlayer(Args[1],p)
---            if Args[2] == nil then
---                p.Character:SetPrimaryPartCFrame(Target.Character.HumanoidRootPart.CFrame)
---            else
---                local Target2 = Api:GetPlayer(Args[2],p)
---               Target.Character:SetPrimaryPartCFrame(Target2.Character.HumanoidRootPart.CFrame)
---            end
---        else
---            -- Invalid Rank Notification
---            Api:InvalidPermissionsNotification(p)
---        end
---    end)
---    if not s then
---        warn(e)
---    end
---
---end)
+end,{"*[player],[player]"},{"tp"})
 
 Api:RegisterCommand("bring","Teleports player to calling player.",function(p,Args)
     local s,e = pcall(function()
